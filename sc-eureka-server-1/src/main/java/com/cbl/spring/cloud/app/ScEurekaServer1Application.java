25827d18dda7a583203907d408129469469f68ad
changbaolong <13940572922@163.com>
2019-10-28 18:03:58 +0800

    集成eureka-server

--- sc-eureka-server-1/src/main/java/com/cbl/spring/cloud/app/ScEurekaServer1Application.java
+++ sc-eureka-server-1/src/main/java/com/cbl/spring/cloud/app/ScEurekaServer1Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ScEurekaServerApplication {
+public class ScEurekaServer1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScEurekaServerApplication.class, args);
+        SpringApplication.run(ScEurekaServer1Application.class, args);
     }
 
 }
