295189af9b06a65953f0a72225c1feba2fb85282
changbaolong <13940572922@163.com>
2019-10-31 20:42:42 +0800

    eureka自定义元数据

--- sc-server-provider/src/main/java/com/cbl/spring/cloud/controller/UserController.java
+++ sc-server-provider/src/main/java/com/cbl/spring/cloud/controller/UserController.java
@@ -2,11 +2,14 @@ package com.cbl.spring.cloud.controller;
 
 import com.cbl.spring.cloud.entity.UserInfo;
 import com.cbl.spring.cloud.service.UserService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -14,9 +17,20 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/users/{userId}")
     public UserInfo getUserById(@PathVariable String userId) {
         return userService.getUserById(userId);
+    }
+
+    /**
+     * 查询服务信息
+     */
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showServerInfo() {
+        return discoveryClient.getInstances("sc-server-provider");
     }
 
 }
